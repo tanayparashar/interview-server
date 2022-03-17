19d534a2d45bfdde22f1713259a538789e0ccc25
Tanay Parashar <tanay@patch.localdomain>
2022-03-17 15:31:51 +0530

    collision addressed

--- routes/candidates.js
+++ routes/candidates.js
@@ -10,36 +10,82 @@ const App=express();
 App.use(cors());
 App.use(bodyParser.json());
 App.options('/', cors());
-App.post('/',(req,res)=>{
-    console.log(req.body);
-    const candidate=new Candidate({
-        _id:new mongoose.Types.ObjectId(),
-        candidateEmail: req.body.candidateEmail,
-        candidateName: req.body.candidateName,
-        interviewerName: req.body.interviewerName,
-        interviewerEmail:req.body.interviewerEmail,
-        startTime: req.body.startTime,
-        endTime:req.body.endTime
-    });
-    candidate.save().then(result => {
-        console.log(result);
-        res.status(201).json(
-        {
-            message:"candidatae created",
-            createdCandidate:candidate
-        }
-    );
-    })
-    .catch(err=>
-        {
-            console.log(error)
-            res.status(500).json(
-                {
-                    message:"error adding candidates to database"
-                }
-            )
-        });
+App.post('/',async (req,res)=>{
+    const flag1=req.body.candidateEmail;
+    var findarr=[];
+    var concide=false;
+    // Candidate.find({ candidateEmail:flag1 }, function (err, docs) {
+    //     if (err){
+    //         console.log(err);
+    //     }
+    //     else{
+    //         console.log(docs);
+    //         findarr=docs;
+    //     }
+    // });
+    let docs =await Candidate.find({ candidateEmail:flag1 })
+    findarr=docs;
     
+    // .then( (data) => (findarr=(data)) );
+    
+    findarr.forEach( (ele)=>{
+        let s=ele.startTime;
+        let e=ele.endTime;
+        console.log(s,e);
+        if(s<=req.body.startTime  && req.body.startTime<=e)
+        {
+            concide=true;
+        }
+        if(s<=req.body.endTime  && req.body.endTime<=e)
+        {
+            concide=true;
+        }
+        if(s>=req.body.startTime  &&  req.body.endTime>=e)
+        {
+            concide=true;
+        }
+    });
+    console.log(concide);
+    if(concide===true)
+    {
+        //console.log("concides");
+        return res.status(200).json(
+            {
+                message:"Start or End Time Coincide"
+            }
+        );
+    }
+    else
+    {
+        //console.log("all good");
+        const candidate=new Candidate({
+            _id:new mongoose.Types.ObjectId(),
+            candidateEmail: req.body.candidateEmail,
+            candidateName: req.body.candidateName,
+            interviewerName: req.body.interviewerName,
+            interviewerEmail:req.body.interviewerEmail,
+            startTime: req.body.startTime,
+            endTime:req.body.endTime
+        });
+        candidate.save().then(result => {
+            console.log(result);
+            res.status(201).json(
+            {
+                message:"candidatae created",
+                createdCandidate:candidate
+            }
+        );
+        })
+        .catch(err=>
+            {
+                console.log(error)
+                res.status(500).json(
+                    {
+                        message:"error adding candidates to database"
+                    }
+                )
+            });
+    }
 });
 
 
@@ -49,7 +95,7 @@ App.get("/",(req,res)=>{
     Candidate.find()
     .exec()
     .then( docs =>{
-            console.log(docs);
+            //console.log(docs);
             res.status(200).json(docs);
     })
     .catch( err => {
@@ -59,7 +105,7 @@ App.get("/",(req,res)=>{
                 message:"error retreiving candidates from database"
             }
         )
-    })
+    });
 });
 
 module.exports=App;
